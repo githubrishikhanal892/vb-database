b5fabc9d964628bcc214b2fac6a8b876f510378d
githubrishikhanal892 <52958168+githubrishikhanal892@users.noreply.github.com>
2019-07-16 17:53:13 +0545

    Add files via upload

---
+++ DatabaseExample/DatabaseExample/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Txtname = New System.Windows.Forms.TextBox()
+        Me.TxtAddress = New System.Windows.Forms.TextBox()
+        Me.Add = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Address"
+        '
+        'Txtname
+        '
+        Me.Txtname.Location = New System.Drawing.Point(106, 28)
+        Me.Txtname.Name = "Txtname"
+        Me.Txtname.Size = New System.Drawing.Size(100, 20)
+        Me.Txtname.TabIndex = 2
+        '
+        'TxtAddress
+        '
+        Me.TxtAddress.Location = New System.Drawing.Point(106, 86)
+        Me.TxtAddress.Name = "TxtAddress"
+        Me.TxtAddress.Size = New System.Drawing.Size(100, 20)
+        Me.TxtAddress.TabIndex = 3
+        '
+        'Add
+        '
+        Me.Add.Location = New System.Drawing.Point(117, 147)
+        Me.Add.Name = "Add"
+        Me.Add.Size = New System.Drawing.Size(75, 23)
+        Me.Add.TabIndex = 4
+        Me.Add.Text = "Add"
+        Me.Add.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(263, 28)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(337, 199)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 323)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Add)
+        Me.Controls.Add(Me.TxtAddress)
+        Me.Controls.Add(Me.Txtname)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Txtname As System.Windows.Forms.TextBox
+    Friend WithEvents TxtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents Add As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
---
+++ DatabaseExample/DatabaseExample/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+
+    Private Sub Add_Click(sender As Object, e As EventArgs) Handles Add.Click
+        Dim name, address As String
+        name = Txtname.Text
+        address = TxtAddress.Text
+        ' Dim constring As OleDbConnection
+        'Dim query As OleDbCommand = New OleDbCommand
+        'constring = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\database\College.accdb")
+        ' Try
+        '   query.Connection = constring
+        '  constring.Open()
+
+        'query.CommandText = "insert into Student(Name,Address)Values(@name,@address)"
+        'query.Parameters.AddWithValue("@name", name)
+        'query.Parameters.AddWithValue("@address", address)
+        'query.ExecuteNonQuery()
+        'Dim retrievequery = "select *from Student"
+        'Dim adtp As OleDbDataAdapter = New OleDbDataAdapter(retrievequery, constring)
+        'Dim ds As DataSet = New DataSet
+        'adtp.Fill(ds, "Student")
+        'DataGridView1.DataSource = ds.Tables(0)
+        ''   MessageBox.Show("data added")
+        'constring.Close()
+        'Catch ex As Exception
+        '    MsgBox("Can not open connection ! ")
+        'End Try
+        Dim data As DataSet
+        Dim database As Databse = New Databse
+        data = database.Insert(name, address)
+        DataGridView1.DataSource = data.Tables(0)
+        ''   If (res = -1) Then
+        'MessageBox.Show("Data can not be inserted")
+        'Else
+        'MessageBox.Show("Data inserted")
+        'End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Dim constring As OleDbConnection
+        'Dim query As OleDbCommand = New OleDbCommand
+        'constring = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\database\College.accdb")
+        'Try
+        'query.Connection = constring
+        'constring.Open()
+        'Dim retrievequery = "select *from Student"
+        'Dim adtp As OleDbDataAdapter = New OleDbDataAdapter(retrievequery, constring)
+        'Dim ds As DataSet = New DataSet
+        'adtp.Fill(ds, "Student")
+        'DataGridView1.DataSource = ds.Tables(0)
+        '   MessageBox.Show("data added")
+        'constring.Close()
+        'Catch ex As Exception
+        'MsgBox("Can not open connection ! ")
+        'End Try
+        Dim alldata As Databse = New Databse
+        Dim retrieveddata As DataSet
+        retrieveddata = alldata.FormData()
+        DataGridView1.DataSource = retrieveddata.Tables(0)
+
+    End Sub
+End Class
